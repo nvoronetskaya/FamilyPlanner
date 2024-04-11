0c06ee45737061a0755320f6963ce92764509011
nvoronetskaya <nivoronetskaya@gmail.com>
2024-04-12 02:58:34 +0300

    Template for edit task fragment

--- app/src/main/java/com/familyplanner/tasks/view/EditTaskFragment.kt
+++ app/src/main/java/com/familyplanner/tasks/view/EditTaskFragment.kt
@@ -1,4 +1,223 @@
 package com.familyplanner.tasks.view
 
-class EditTaskFragment {
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.os.Bundle
+import android.provider.OpenableColumns
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.familyplanner.MainActivity
+import com.familyplanner.R
+import com.familyplanner.databinding.FragmentNewTaskBinding
+import com.familyplanner.tasks.adapters.FileAdapter
+import com.familyplanner.tasks.model.Status
+import com.familyplanner.tasks.model.UserFile
+import com.familyplanner.tasks.viewmodel.EditTaskViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.mapview.MapView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Calendar
+
+class EditTaskFragment : Fragment() {
+    private var _binding: FragmentNewTaskBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: EditTaskViewModel
+    private var calendar: Calendar = Calendar.getInstance()
+    private var bottomSheet: BottomSheetDialog? = null
+    private val ATTACH_FILES = 10
+    private lateinit var filesAdapter: FileAdapter
+    private var curPoint: Point? = null
+    private lateinit var userId: String
+    private lateinit var familyId: String
+    private var parentId: String? = null
+
+    private val inputListener = object : InputListener {
+        override fun onMapTap(p0: Map, p1: Point) {
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    viewModel.getAddressByGeo(p1).collect {
+                        if (it == Status.SUCCESS) {
+                            activity?.runOnUiThread {
+                                curPoint = p1
+                                binding.tvAddress.visibility = View.VISIBLE
+                                binding.tvAddress.text = viewModel.getAddress()
+                                bottomSheet?.cancel()
+                            }
+                        } else {
+                            activity?.runOnUiThread {
+                                Toast.makeText(
+                                    activity,
+                                    "Ошибка. Проверьте подключение к сети и повторите позднее",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun onMapLongTap(p0: Map, p1: Point) {
+        }
+    }
+
+    override fun onStop() {
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[EditTaskViewModel::class.java]
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ATTACH_FILES) {
+            if (resultCode == Activity.RESULT_OK) {
+                val uri = data?.data
+                if (uri == null) {
+                    Toast.makeText(activity, "Не удалось прикрепить файл", Toast.LENGTH_SHORT)
+                        .show()
+                    return
+                }
+                requireActivity().contentResolver.query(uri, null, null, null, null).use { cursor ->
+                    val nameIndex = cursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                    val sizeIndex = cursor!!.getColumnIndex(OpenableColumns.SIZE)
+                    cursor.moveToFirst()
+                    val name = cursor.getString(nameIndex)
+                    val size = cursor.getDouble(sizeIndex)
+                    filesAdapter.addFile(UserFile(uri, name, size))
+                }
+            }
+        }
+    }
+
+    private fun setDeadline(calledBySwitch: Boolean = false) {
+        val dialog = DatePickerDialog(
+            activity as MainActivity,
+            R.style.datePickerDialog,
+            { _, year, month, day ->
+                binding.tvDeadline.text = String.format("%02d.%02d.%d", day, month + 1, year)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        dialog.setOnCancelListener {
+            if (calledBySwitch) {
+                binding.swDeadline.isChecked = false
+            }
+        }
+        dialog.datePicker.minDate = calendar.timeInMillis
+        dialog.show()
+    }
+
+    private fun setStartDate() {
+        val dialog = DatePickerDialog(
+            activity as MainActivity,
+            R.style.datePickerDialog,
+            { _, year, month, day ->
+                binding.tvRepeatStart.text = String.format("%02d.%02d.%d", day, month + 1, year)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        dialog.datePicker.minDate = calendar.timeInMillis
+        dialog.show()
+    }
+
+    private fun setTime(isStartTime: Boolean, calledByCheckbox: Boolean = false) {
+        val dialog = TimePickerDialog(
+            activity,
+            R.style.datePickerDialog,
+            { _, hour, minute ->
+                if (isStartTime) {
+                    binding.tvStartValue.text = String.format("%02d:%02d", hour, minute)
+
+                    if (binding.tvFinishTime.text.isNullOrBlank() || getTimeFromString(binding.tvFinishValue.text.toString()) < hour * 60 + minute) {
+                        binding.tvFinishValue.text = String.format("%02d:%02d", hour, minute)
+                    }
+                } else {
+                    binding.tvFinishValue.text = String.format("%02d:%02d", hour, minute)
+                    if (binding.tvStartValue.text.isNullOrBlank() || getTimeFromString(binding.tvStartValue.text.toString()) > hour * 60 + minute) {
+                        binding.tvStartValue.text = String.format("%02d:%02d", hour, minute)
+                    }
+                }
+
+                if (calledByCheckbox && isStartTime) {
+                    setTime(isStartTime = false, calledByCheckbox = true)
+                }
+            },
+            12,
+            0,
+            true
+        )
+        dialog.setOnCancelListener {
+            if (calledByCheckbox) {
+                binding.cbContinuousTask.isChecked = false
+            }
+        }
+        dialog.show()
+    }
+
+    private fun getTimeFromString(textTime: String): Int {
+        if (textTime.isBlank()) {
+            return -1
+        }
+
+        val parts = textTime.split(':').map { it.toInt() }
+        return parts[0] * 60 + parts[1]
+    }
+
+    private fun showBottomSheet() {
+        val bottomSheet = BottomSheetDialog(activity as MainActivity)
+        bottomSheet.setContentView(R.layout.bottomsheet_map)
+        val map = bottomSheet.findViewById<MapView>(R.id.map)?.mapWindow?.map ?: return
+
+        map.addInputListener(inputListener)
+        bottomSheet.setOnDismissListener {
+            if (binding.tvAddress.text.isNullOrBlank()) {
+                binding.swHasLocation.isChecked = false
+            }
+        }
+
+        bottomSheet.behavior.isDraggable = false
+        this.bottomSheet = bottomSheet
+        bottomSheet.show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
