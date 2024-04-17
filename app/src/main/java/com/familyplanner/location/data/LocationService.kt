58644ff06db6920ba494fea27dc7a562c600142a
nvoronetskaya <nivoronetskaya@gmail.com>
2024-04-17 17:07:02 +0300

    Add location template

--- app/src/main/java/com/familyplanner/location/data/LocationService.kt
+++ app/src/main/java/com/familyplanner/location/data/LocationService.kt
@@ -1,4 +1,60 @@
 package com.familyplanner.location.data
 
-class LocationService {
+import android.Manifest
+import android.app.Service
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.IBinder
+import android.os.Looper
+import androidx.core.app.ActivityCompat
+import com.familyplanner.FamilyPlanner
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+
+
+class LocationService : Service() {
+    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 3000
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private val locationSettingsRequest: LocationSettingsRequest? = null
+    private val locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val currentLocation: Location? = locationResult.lastLocation
+//            Log.d("Locations", currentLocation.getLatitude() + "," + currentLocation.getLongitude())
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        locationRequest = LocationRequest.Builder(UPDATE_INTERVAL_IN_MILLISECONDS).build()
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mFusedLocationClient.requestLocationUpdates(
+                this.locationRequest,
+                this.locationCallback,
+                Looper.myLooper()
+            )
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 }
