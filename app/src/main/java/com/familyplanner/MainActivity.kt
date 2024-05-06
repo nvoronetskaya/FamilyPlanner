c30a534b70e78896305c2bb4b170aecaa3cb60c3
nvoronetskaya <nivoronetskaya@gmail.com>
2024-05-06 19:49:23 +0300

    Fixes

--- app/src/main/java/com/familyplanner/MainActivity.kt
+++ app/src/main/java/com/familyplanner/MainActivity.kt
@@ -2,41 +2,23 @@ package com.familyplanner
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
-import android.graphics.PorterDuff
 import android.net.ConnectivityManager
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
-import androidx.core.view.iterator
-import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
-import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import com.familyplanner.auth.view.WelcomeFragment
 import com.familyplanner.databinding.ActivityMainBinding
-import com.familyplanner.family.view.NoFamilyFragment
 import com.familyplanner.location.data.LocationService
 import com.google.firebase.Firebase
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.auth
-import com.google.firebase.firestore.firestore
 import com.yandex.mapkit.MapKitFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -50,18 +32,17 @@ class MainActivity : AppCompatActivity() {
             MapKitFactory.setApiKey("20c53eda-cff4-4d4e-bbac-f2d4a5cda330")
         }
         MapKitFactory.initialize(this)
-
         supportActionBar?.hide()
         viewModel = ViewModelProvider(this)[ActivityViewModel::class.java]
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.bottomNavigation.visibility = View.GONE
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         navController = navHostFragment.navController
-        if (Firebase.auth.currentUser != null) {
+        val isWelcomeFragment = navController.currentDestination?.id == R.id.welcomeFragment
+        binding.bottomNavigation.isVisible = !isWelcomeFragment
+        if (Firebase.auth.currentUser != null && isWelcomeFragment) {
             navController.navigate(R.id.action_welcomeFragment_to_noFamilyFragment)
             binding.bottomNavigation.visibility = View.VISIBLE
         }
@@ -131,8 +112,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun isConnectedToInternet(): Boolean {
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            ?: return false
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = connectivityManager.activeNetworkInfo
         return network != null && network.isConnected
     }
